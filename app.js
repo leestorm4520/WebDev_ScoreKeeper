37e4d59d6a79306e0eda785b51910275da1a5e1d
DuyLe <minhduy4520@gmail.com>
2022-07-20 14:31:37 -0500

    update

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
 function score(id){
-    const p=document.getElementById('id');
-    static s1;
-    static s2;
-    if(p==='p1') s1++;
-    if(p==='p2') s2++;
-    const o=document.getElementById("total");
-    const total= parseInt(o.options[o.selectedIndex].text);
-    const p1=document.getElementById("p1");
-    const p2=document.getElementById("p2");
-    if(s1+s2>total){
-        p1.disabled=true;
-        p2.disabled=true;
-    }
-    else{
-        p1.disabled=false;
-        p2.disabled=false;
-    }
-    const point=document.getElementsByTagName('h1')[0];
-    point.innerText=s1+' to '+s2;
-}
-
-
+  
 
